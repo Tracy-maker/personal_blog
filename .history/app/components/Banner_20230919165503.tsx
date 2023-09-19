baba22f98790c155d152d21a484ca0a33d3daecf
Tracy-maker <ydlvns@gmail.com>
2023-09-19 17:02:51 +1000

    add the banner file

--- .history/app/components/Banner_20230919165503.tsx
+++ .history/app/components/Banner_20230919165503.tsx
@@ -3,11 +3,9 @@ function Banner() {
     <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 p-5 mb-10">
       <div>
         <h1 className="text-6xl">Hang Gao&apos;s Personal Blogs</h1>
-        <h3 className="mt-5 md:mt-2 text-orange-600">
-          Step right up to the canvas of curiosity! This site not only peddles
-          oil paintings but serves up a side of my blog humor. Thanks for
-          popping in – let&apos;s brush up on some fun together! 🎨😄
-        </h3>
+        <h2 className="mt-5 md:mt-2 text-orange-600">
+        Step right up to the canvas of curiosity! This site not only peddles oil paintings but serves up a side of my blog humor. Thanks for popping in – let's brush up on some fun together! 🎨😄
+        </h2>
       </div>
       <p className="mt-5 md:mt-2 text-gray-400 max-w-sm">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, itaque,
